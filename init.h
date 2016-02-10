603291514e520cce7f11db40a390314c6e22a9c0
Matthew Leach <matthew@mattleach.net>
2016-02-10 18:16:51 +0000

    init: make initcall definitions volatile
    When defining an initcall, mark it as volatile, preventing the compiler
    optimising it away.

--- init.h
+++ init.h
@@ -2,8 +2,8 @@
 
 typedef void (*initcall_t)(void);
 
-#define __define_initcall(fn, lvl)                      \
-    static initcall_t __initcall_##fn##lvl              \
+#define __define_initcall(fn, lvl)                              \
+    static volatile initcall_t __initcall_##fn##lvl             \
     __attribute__((__section__(".initcall_" #lvl))) = fn;
 
 #define early_initcall(fn) __define_initcall(fn, early)
