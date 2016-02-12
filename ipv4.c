868293dc79e1cc984be539a676d0b474e85394a1
Matthew Leach <matthew@mattleach.net>
2016-02-12 22:01:38 +0000

    ipv4: new
    Add a basic ipv4 implementation, this adds the xmit path only.

---
+++ ipv4.c
@@ -0,0 +1,67 @@
+#include "ipv4.h"
+#include "byteswap.h"
+#include "arp.h"
+#include "ethernet.h"
+#include "memory.h"
+#include <string.h>
+
+#define DEFAULT_TTL 10
+
+static void ip4_swap_endian(ip4_header *iphdr)
+{
+    swap_endian16(&iphdr->tot_length);
+    swap_endian16(&iphdr->identification);
+    swap_endian32(&iphdr->src_ip);
+    swap_endian32(&iphdr->dst_ip);
+}
+
+static void ip4_compute_checksum(ip4_header *header)
+{
+    uint16_t *x = (uint16_t *)header;
+    uint32_t sum = 0;
+    int i;
+
+    for (i = 0; i < sizeof(ip4_header) / 2; i++)
+        sum += x[i];
+
+    while (sum >> 16)
+        sum = (sum & 0xffff) + (sum >> 16);
+
+    header->header_checksum = ~sum;
+}
+
+void ip4_xmit_packet(uint8_t protocol, uint32_t dst_ip, void *payload,
+                     uint16_t payload_len)
+{
+    void *packet_buf;
+    int packet_buf_len = sizeof(ip4_header) + payload_len;
+    ip4_header *header;
+    uint8_t *dst_hw_addr = resolve_address(dst_ip);
+
+    if (!dst_hw_addr)
+        return;
+
+    packet_buf = get_mem(packet_buf_len);
+    memset(packet_buf, 0, packet_buf_len);
+
+    header = (ip4_header *)packet_buf;
+
+    header->version = 4;
+    header->ihl = 5;
+    header->tot_length = packet_buf_len;
+    header->ttl = DEFAULT_TTL;
+    header->protocol = protocol;
+    header->src_ip = OUR_IP_ADDRESS;
+    header->dst_ip = dst_ip;
+
+    ip4_swap_endian(header);
+
+    ip4_compute_checksum(header);
+
+    memcpy(packet_buf + sizeof(*header), payload, payload_len);
+
+    ether_xmit_payload(dst_hw_addr, ETHERTYPE_IP, packet_buf,
+                       packet_buf_len);
+
+    free_mem(header);
+}
---
+++ ipv4.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdint.h>
+
+#define IP_PROTO_TCP 0x6        /* Ambitious! */
+#define IP_PROTO_UDP 0x11
+
+typedef struct {
+    uint8_t ihl : 4;
+    uint8_t version : 4;
+    uint8_t __reserved_0;
+    uint16_t tot_length;
+    uint16_t identification;
+    uint16_t __reserverd_1;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint32_t src_ip;
+    uint32_t dst_ip;
+} __attribute__((packed)) ip4_header;
+
+void ip4_xmit_packet(uint8_t protocol, uint32_t dst_ip, void *payload,
+                     uint16_t payload_len);
