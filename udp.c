245babf0888931520898d0bb4f69736fbd000310
Matthew Leach <matthew@mattleach.net>
2016-11-26 17:25:59 +0000

    udp: add rx functionality
    Add the ability for userspace to recieve UDP packets.

--- udp.c
+++ udp.c
@@ -1,8 +1,24 @@
 #include "udp.h"
+#include "atomics.h"
 #include "ipv4.h"
 #include "byteswap.h"
 #include "memory.h"
+#include "list.h"
+#include "error.h"
+#include "wait.h"
 #include <string.h>
+
+typedef struct
+{
+    uint8_t *dst_buf;
+    uint16_t dst_buf_sz;
+    uint16_t dst_buf_ptr;
+    uint16_t port;
+    list rx_requests;
+}udp_listener;
+
+static LIST(udp_rx_requests);
+mutex_t udp_rx_requests_lock;
 
 static void udp_swap_endian(udp_header *header)
 {
@@ -10,9 +26,63 @@ static void udp_swap_endian(udp_header *header)
     swap_endian16(&header->length);
 }
 
+int udp_rx(uint16_t port, void *dst_buf, uint16_t dst_buf_sz)
+{
+    udp_listener *i;
+    udp_listener newListener;
+
+    get_lock(&udp_rx_requests_lock);
+
+    list_for_each(i, &udp_rx_requests, rx_requests)
+        if (i->port == port)
+            return -EINUSE;
+
+    newListener.port = port;
+    newListener.dst_buf = dst_buf;
+    newListener.dst_buf_sz = dst_buf_sz;
+    newListener.dst_buf_ptr = 0;
+
+    list_add(&newListener.rx_requests, &udp_rx_requests);
+
+    release_lock(&udp_rx_requests_lock);
+
+    wait_for_volatile_condition(newListener.dst_buf_ptr ==
+                                newListener.dst_buf_sz);
+
+    get_lock(&udp_rx_requests_lock);
+    list_del(&newListener.rx_requests);
+    release_lock(&udp_rx_requests_lock);
+
+    return newListener.dst_buf_ptr;
+}
+
 void udp_rx_packet(uint32_t dst_ip, void *payload, int payload_len)
 {
-    return;
+    udp_listener *i;
+    udp_header *header = (udp_header *)payload;
+    uint8_t *udp_payload = (payload + sizeof(udp_header));
+
+    udp_swap_endian(header);
+
+    size_t udp_payload_sz = header->length - sizeof(udp_header);
+
+    if (try_lock(&udp_rx_requests_lock)) {
+        list_for_each(i, &udp_rx_requests, rx_requests) {
+            if (i->port == header->dst_port) {
+                int user_buf_sz = i->dst_buf_sz - i->dst_buf_ptr,
+                    no_bytes_to_copy = user_buf_sz;
+
+
+                if (udp_payload_sz < no_bytes_to_copy)
+                    no_bytes_to_copy = udp_payload_sz;
+
+                memcpy(i->dst_buf + i->dst_buf_ptr, udp_payload, no_bytes_to_copy);
+
+                i->dst_buf_ptr += no_bytes_to_copy;
+            }
+        }
+        release_lock(&udp_rx_requests_lock);
+    }
 }
 
 void udp_xmit_packet(uint16_t dst_port, uint32_t dst_ip, void *payload,
--- udp.h
+++ udp.h
@@ -8,6 +8,7 @@ typedef struct {
     uint16_t checksum;
 } udp_header;
 
+int udp_rx(uint16_t port, void *dst_buf, uint16_t dst_buf_sz);
 void udp_rx_packet(uint32_t dst_ip, void *payload, int payload_len);
 
 void udp_xmit_packet(uint16_t dst_port, uint32_t dst_ip, void *payload,
